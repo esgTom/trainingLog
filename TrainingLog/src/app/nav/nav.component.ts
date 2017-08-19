c46432da409a8c8fafa86b124ca6e7d22a37ebdf
esgTom <esgTom@entara.com>
2017-08-19 17:43:36 -0500

    Adding Bootstrap and Fontawesone

---
+++ TrainingLog/src/app/nav/nav.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'training-log-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
